3426d46700fd7164b4a6b265a24ad09daf8148b0
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-11-15 10:56:09 -0500

    using receive and after instead of sleeping

--- lib/pomodoro.ex
+++ lib/pomodoro.ex
@@ -4,21 +4,32 @@ defmodule Pomodoro do
   end
 
   def _timer(start_seconds, current_minutes) do
+
+    receive do
+      {:wut, pid} -> IO.puts "wut"
+
+      after 250 ->
+      elapsed_minutes = get_elapsed_minutes
+  
+      case elapsed_minutes > current_minutes do
+        true when rem(elapsed_minutes, 5) == 0 ->
+          IO.write "[#{elapsed_minutes}] "
+          _timer(start_seconds, elapsed_minutes)
+        true -> 
+          IO.write "."
+          _timer(start_seconds, elapsed_minutes)
+        false -> _timer(start_seconds, elapsed_minutes)
+      end
+
+      _timer(start_seconds, current_minutes)
+
+    end
+
+  end
+
+  defp get_elapsed_minutes do
     now_seconds = elem(:erlang.now, 1)
     elapsed_seconds = now_seconds - start_seconds
     elapsed_minutes = round Float.floor(elapsed_seconds / 60)
-
-    case elapsed_minutes > current_minutes do
-      true when rem(elapsed_minutes, 5) == 0 ->
-        IO.write "[#{elapsed_minutes}] "
-        _timer(start_seconds, elapsed_minutes)
-      true -> 
-        IO.write "."
-        _timer(start_seconds, elapsed_minutes)
-      false -> _timer(start_seconds, elapsed_minutes)
-    end
-
-#    :timer.sleep(250)
-    _timer(start_seconds, current_minutes)
   end
 end
