8334ac3072b27265f1c22795b2a038cf13a6dab9
Mark Haskamp <mark.haskamp@pinnsg.com>
2014-11-17 19:08:03 -0500

    counting for a pomodoro

--- lib/pomodoro.ex
+++ lib/pomodoro.ex
@@ -4,12 +4,15 @@ defmodule Pomodoro do
   def start do
     pid = spawn(__MODULE__, :receive_loop, [])
     :global.register_name(PomoRcvLoop, pid)
+
+    Agent.start_link(fn -> {elem(:erlang.now, 1), 0, :s} end, name: PomoState)
   end
 
   def receive_loop do
     receive do
       { "p", label} ->
-        IO.puts "pomodoro!"
+        IO.puts "\n#{label}"
+        Agent.update(PomoState, fn(s) -> {elem(:erlang.now, 1), 0, :p} end)
 
       {"s", label} ->
         IO.puts "Stop!"
@@ -21,40 +24,31 @@ defmodule Pomodoro do
         IO.puts "uhh, wut"
 
       after 3000 ->
-        IO.write "."
+        {start_seconds, current_minutes, state} = Agent.get(PomoState, fn(s) -> s end)
+        if (state == :p) do
+          elapsed_minutes = get_elapsed_minutes(start_seconds)
+          if (elapsed_minutes > current_minutes), do: write_to_screen(elapsed_minutes)
+        end
+
+        Agent.update(PomoState, fn(s) -> {start_seconds, elapsed_minutes, state} end)
     end
 
     receive_loop
   end
-#  def start do
-#    _timer(elem(:erlang.now, 1), 0)
-#  end
-#
-#  def _timer(start_seconds, current_minutes) do
-#
-#    receive do
-#      {:wut, pid} -> IO.puts "wut"
-#
-#      after 250 ->
-#        elapsed_minutes = get_elapsed_minutes(start_seconds)
-#        if (elapsed_minutes > current_minutes), do: write_to_screen(elapsed_minutes)
-#        _timer(start_seconds, elapsed_minutes)
-#    end
-#
-#  end
-#
-#  defp get_elapsed_minutes(start_seconds) do
-#    now_seconds = elem(:erlang.now, 1)
-#    elapsed_seconds = now_seconds - start_seconds
-#    elapsed_minutes = round Float.floor(elapsed_seconds / @write_interval)
-#  end
-#
-#  defp write_to_screen(elapsed_minutes) do
-#    if (rem(elapsed_minutes, 5) == 0) do
-#      IO.write "[#{elapsed_minutes}] "
-#    else
-#      IO.write "."
-#    end
-#  end
+
+
+  defp get_elapsed_minutes(start_seconds) do
+    now_seconds = elem(:erlang.now, 1)
+    elapsed_seconds = now_seconds - start_seconds
+    elapsed_minutes = round Float.floor(elapsed_seconds / @write_interval)
+  end
+
+  defp write_to_screen(elapsed_minutes) do
+    if (rem(elapsed_minutes, 5) == 0) do
+      IO.write "[#{elapsed_minutes}] "
+    else
+      IO.write "."
+    end
+  end
 
 end
